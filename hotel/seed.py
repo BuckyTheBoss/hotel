ee4c4d2bdcc2d18337e4f9b9a09df4bfebef41c7
Avi <buchman.avi@gmail.com>
2019-04-17 15:39:18 +0300

    added review model and updated a new seed function for it

--- hotel/seed.py
+++ hotel/seed.py
@@ -16,11 +16,22 @@ def gen_lname():
 def gen_password():
 	return fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
 
-def pick_user():
+def gen_paragraph():
+	return fake.paragraph(nb_sentences=5, variable_nb_sentences=True, ext_word_list=None)
+
+def gen_sentance():
+	return fake.sentence(nb_words=6, variable_nb_words=True, ext_word_list=None)
+
+def pick_user(booking=False):
+	if booking:
+		return random.choice(User.objects.filter(booking__isnull=False).all())
 	return random.choice(User.objects.all())
 
 def pick_room():
 	return random.choice(Room.objects.all())
+
+def pick_booking(user):
+	return random.choice(Booking.objects.filter(user=user).all())
 
 def create_users(number):
 	for i in range(0, number):
@@ -51,6 +62,7 @@ def seed_rooms(number):
 		room.save()
 
 def seed_booking(number):
+	bookings = []
 	for i in range(0,number):
 		room = pick_room()
 		start_date = gen_datetime()
@@ -61,4 +73,14 @@ def seed_booking(number):
 			end_date = gen_datetime(start_date),
 			occupants = (room.room_type.capacity - 1)
 			)
-		booking.save()
+		bookings.append(booking)
+	Booking.objects.bulk_create(bookings)
+
+def seed_reviews(number):
+	reviews = []
+	for i in range(0,number):
+		user = pick_user(booking=True)
+		booking = pick_booking(user)
+		review = Review(title = gen_sentance(),content = gen_paragraph(),booking = booking,user = user)
+		reviews.append(review)
+	Review.objects.bulk_create(reviews)
