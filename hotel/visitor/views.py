a84f8c1f655794915e3ee61cc369cec3007f7dbc
oz kilim <ozkilim@hotmail.co.uk>
2019-04-17 09:44:29 +0100

    thrusday morning update oz

--- hotel/visitor/views.py
+++ hotel/visitor/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'index.html')
+
+def hotel(request):
+	return render(request, 'hotel.html')
+
+
+
+def confirm(request):
+	return render(request, 'conformation.html')
+
+
+
+
+	
+def reviews(request):
+	return render(request, 'reviews.html')
