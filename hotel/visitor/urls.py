abf1616c249ee62b47ea633c8cfbc9c64b0bd7c3
Avi <buchman.avi@gmail.com>
2019-04-18 15:15:05 +0300

    fixed, added views and html for reviews, small touches to base.html too

--- hotel/visitor/urls.py
+++ hotel/visitor/urls.py
@@ -14,4 +14,8 @@ urlpatterns = [
    path('review/<int:review_id>', views.review, name='review'),
    path('review/add', views.add_review, name='add_review'),
    path('signup/', views.signup, name='signup'),
+   path('review/<int:review_id>/edit', views.edit_review, name='edit_review'),
+   path('review/<int:review_id>/delete', views.delete_review, name='delete_review'),
+   path('reviews/user/<int:user_id>', views.user_reviews, name='user_reviews'),
+
 ]
--- hotel/visitor/views.py
+++ hotel/visitor/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import *
+
 
 # Create your views here.
 def index(request):
@@ -18,7 +20,7 @@ def confirm(request):
 
 def reviews(request):
 	reviews = Review.objects.all().order_by('-timestamp')
-	return render(request, 'reviews.html', {'reviews' : reviews})
+	return render(request, 'reviews.html', {'reviews' : reviews, 'title': 'All Reviews'})
 
 def review(request, review_id):
 	review = Review.objects.get(pk=review_id)
@@ -27,17 +29,22 @@ def review(request, review_id):
 	# flash message saying review with that id not found
 	return redirect('visitor:reviews')
 
+def user_reviews(request, user_id):
+	user = User.objects.get(pk=user_id)
+	reviews = user.review_set.all()
+	return render(request, 'reviews.html', {'reviews' : reviews, 'title': 'Reviews by: {} {}'.format(user.first_name, user.last_name)})
+
 @login_required
 def add_review(request):
 	if request.method != 'POST':
 		form = ReviewForm()
-		return render(request, 'add_review.html', {'form': form})
+		return render(request, 'add_review.html', {'form': form, 'title' : "Add Review"})
 	form = ReviewForm(request.POST)
 	if form.is_valid():
 		review = form.save(commit=False)
 		review.user = request.user
 		review.save()
-		return redirect('review',review.id)
+		return redirect('visitor:review',review.id)
 
 def signup(request):
     if request.method == 'POST': # If the form has been submitted
@@ -55,3 +62,29 @@ def signup(request):
 
     form = UserCreationForm() # An unbound form
     return render(request, 'signup.html', {'form': form})
+
+
+@login_required
+def edit_review(request, review_id):
+	review = Review.objects.get(pk=review_id)
+	if request.user != review.user:
+		#flash message saying "not your review to edit"
+		return redirect('visitor:review', review.id)
+	if request.method != 'POST':
+		form = ReviewForm(instance=review)
+		return render(request, 'add_review.html', {'form': form, 'title' : "Edit Review"})
+	form = ReviewForm(request.POST, instance=review)
+	if form.is_valid():
+		review.save()
+		return redirect('visitor:review',review.id)
+	return redirect('visitor:index')
+
+@login_required
+def delete_review(request, review_id):
+	review = Review.objects.get(pk=review_id)
+	if request.user == review.user:
+		review.delete()
+		#flash message saying review deleted
+		return redirect('visitor:reviews')
+	#flash message saying "not your review to delete"
+	return redirect('visitor:review', review.id)
